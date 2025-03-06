987a57e3e71b9b93bf3609926996aa8bfc847eb7
skemb89 <skemb89@gmail.com>
2025-03-06 21:21:34 +0100

    Agg foto profilo

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000; // Usa la porta definita da Render o 5000 di fallback
